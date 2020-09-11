8a723dec2e7b2cf1b61f66baa74117614bdeed40
Ashutosh Joshi <ashutoshj590@gmail.com>
2020-09-11 10:45:41 +0530

    change  header for https

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(cors())
 
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // http://localhost:4200
+    res.header("Access-Control-Allow-Origin", "https://www.mccpapp.com"); // http://localhost:4200
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); 
     next();
   });
