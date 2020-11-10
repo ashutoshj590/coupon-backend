ab63546f4af5a658a3699591af18ca12e1cd4044
Ashutosh Joshi <ashutoshj590@gmail.com>
2020-11-10 10:37:43 +0530

    change url in passport

--- services/passport-setup.js
+++ services/passport-setup.js
@@ -12,7 +12,7 @@ passport.serializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
     clientID: "47163792352-vo9t9bc1r60mqss8j72bt9h69dimeasm.apps.googleusercontent.com",
     clientSecret: "prv7WfnRXs5sPyI_4ESwPx8t",
-    callbackURL: "http://localhost:8080/google/callback" //https://www.mccpapp.com:8080/google/callback   //http://localhost:8080/google/callback
+    callbackURL: "https://www.mccpapp.com:8080/google/callback" //https://www.mccpapp.com:8080/google/callback   //http://localhost:8080/google/callback
   },
   function(accessToken, refreshToken, profile, done) {
       return done(null, profile);
