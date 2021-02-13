63de8aba0446a8cdf62c83366948c9c8d2392097
Ashutosh Joshi <ashutoshj590@gmail.com>
2021-02-13 14:13:12 +0530

    Add deleted method

--- services/couponService.js
+++ services/couponService.js
@@ -415,7 +415,7 @@ exports.getCouponsBySerach = function(search_query){
     var query = 'SELECT Coupons.id as coupon_id,Coupons.user_id as merchant_id,Coupons.coupon_type,Coupons.days,Coupons.start_time,Coupons.end_time,' +
                 'Coupons.expiry_date,Coupons.flash_deal,Coupons.description,Coupons.restriction,Coupons.createdAt,Coupons.updatedAt,Coupons.short_name,Coupons.coupon_code,' +
                 'Registrations.business_name as merchant_name,Registrations.lat,Registrations.lang from Coupons LEFT JOIN Registrations ON Coupons.user_id=Registrations.user_id' +
-                '  where NOT Coupons.coupon_type="custom" AND ( Coupons.short_name like :search_query OR Coupons.description like :search_query )';
+                '  where NOT Coupons.coupon_type="custom" AND Coupons.is_deleted=0 AND ( Coupons.short_name like :search_query OR Coupons.description like :search_query )';
               
   models.sequelize.query(query,
       { replacements: replacements, type: models.sequelize.QueryTypes.SELECT }
