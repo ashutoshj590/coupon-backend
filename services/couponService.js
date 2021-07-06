cea8ed6902f4c61d91cc3ea3c185b688120a2b1c
ashutosh joshi <ashutoshj590@gmail.com>
2021-07-06 22:05:06 +0530

    solve issue of mail and search query

--- services/couponService.js
+++ services/couponService.js
@@ -440,8 +440,9 @@ exports.getCouponsBySerach = function(search_query, consumer_id){
   
     var query = 'SELECT Coupons.id as coupon_id,Coupons.user_id as merchant_id,Coupons.coupon_type,Coupons.days,Coupons.start_time,Coupons.end_time,' +
                 'Coupons.expiry_date,Coupons.flash_deal,Coupons.description,Coupons.restriction,Coupons.createdAt,Coupons.updatedAt,Coupons.short_name,Coupons.coupon_code,' +
-                'Registrations.business_name as merchant_name,Registrations.lat,Registrations.lang from Coupons LEFT JOIN Registrations ON Coupons.user_id=Registrations.user_id' +
-                '  where NOT Coupons.coupon_type="custom" AND Coupons.is_deleted=0 AND ( Coupons.short_name like :search_query OR Coupons.description like :search_query )' + querySet;
+                'Registrations.business_name as merchant_name,Registrations.lat,Registrations.lang,SubCategories.name as category_name from Coupons LEFT JOIN Registrations ON Coupons.user_id=Registrations.user_id' +
+                ' LEFT JOIN SubCategories ON Coupons.sub_category_id=SubCategories.id where NOT Coupons.coupon_type="custom" AND Coupons.is_deleted=0 AND ( Coupons.short_name like :search_query OR Coupons.description like :search_query OR Coupons.coupon_type like :search_query' +
+                ' OR Registrations.business_name like :search_query OR SubCategories.name like :search_query )' + querySet;
               
   models.sequelize.query(query,
       { replacements: replacements, type: models.sequelize.QueryTypes.SELECT }
