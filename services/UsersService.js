be459d97a87a361f6ec8ca3575f132c7b3a6d78c
ashutosh joshi <ashutoshj590@gmail.com>
2021-09-02 01:33:19 +0530

    yes ohh

--- services/UsersService.js
+++ services/UsersService.js
@@ -971,7 +971,7 @@ exports.getAllMerchant = function(){
             axios.get('https://maps.googleapis.com/maps/api/geocode/json?latlng='+data.lat+','+data.lang+'&sensor=true&key='+key)
   .then(function (response) {
     // handle success
-    if (response.data.result[0] != null || undefined ){    
+    if (response.data.result != null || undefined ){    
     data.country_name = response.data.results[0].address_components[5].long_name;
     data.zipcode_new = response.data.results[0].address_components[6].long_name;
     data.formatted_address = response.data.results[0].formatted_address;
