f504c1477746ed5d785882bf6b3ecf753aa6b715
ashutosh joshi <ashutoshj590@gmail.com>
2021-09-02 17:34:38 +0530

    add formated address

--- services/UsersService.js
+++ services/UsersService.js
@@ -973,13 +973,13 @@ exports.getAllMerchant = function(){
     ).then(function(merchants) {
         var output = [];
         async.eachSeries(merchants,function(data,callback){
-          
+        
             axios.get('https://maps.googleapis.com/maps/api/geocode/json?latlng='+data.lat+','+data.lang+'&sensor=true&key='+key)
   .then(function (response) {
       if (response.data.results[0] != null || undefined ){     
-  //  data.country_name = response.data.results[0].address_components[5].long_name;
-   // data.zipcode_new = response.data.results[0].address_components[6].long_name;
-    data.formatted_address = "check the data of lat lang"
+    data.country_name = response.data.results[0].address_components[5].long_name;
+    data.zipcode_new = response.data.results[0].address_components[6].long_name;
+    data.formatted_address = response.data.results[0].formatted_address;
     } else {
         data.country_name = '';
         data.zipcode_new = ''; 
