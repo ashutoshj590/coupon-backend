a6a835f6e41ae687d6e48c3e5f83141c20274d6c
ashutosh joshi <ashutoshj590@gmail.com>
2021-09-02 08:53:07 +0530

    issue resloved in get address by lat lang

--- services/UsersService.js
+++ services/UsersService.js
@@ -971,6 +971,8 @@ exports.getAllMerchant = function(){
             axios.get('https://maps.googleapis.com/maps/api/geocode/json?latlng='+data.lat+','+data.lang+'&sensor=true&key='+key)
   .then(function (response) {
       if (response.data.results[0] != null || undefined ){
+          console.log("if>>>>>>>>.");
+          console.log(response.data.results[0].address_components);
     data.country_name = response.data.results[0].address_components[5].long_name;
     data.zipcode_new = response.data.results[0].address_components[6].long_name;
     data.formatted_address = response.data.results[0].formatted_address;
